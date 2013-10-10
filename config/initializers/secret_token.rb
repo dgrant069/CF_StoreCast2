51e6611df911d4772edce3e0a19f2931650e4d70
Dylan Grant <dgrant069@hotmail.com>
2013-10-10 14:14:10 -0700

    brand new app no changes yet

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreCast2::Application.config.secret_token = 'd5c46f766fb042cdf8445223dfdb4a3fbff4aa3e877dee94ec067f9677b637e24d28417cc5e375dc8f7f183980ccf3dbdcbce471261fe8464642bad0a8360134'
